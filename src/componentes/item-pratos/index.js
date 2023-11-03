35fd8e35ce28fb99c8c5abee2ef68b46a17b7f2c
Renanluizssx <renanluizssx@gmail.com>
2023-11-03 10:51:46 -0300

    commitado

--- src/componentes/item-pratos/index.js
+++ src/componentes/item-pratos/index.js
@@ -1,13 +1,19 @@
 import "./item-pratos.css";
-function ItemPratos({nome, categoria, preco, descricao, imagem}) {
-    return(
-        <div className="div-container-produtos">
-            <img src= {imagem} alt={nome}/>
-            {nome}
-           <p> {categoria}<br/>
-            {descricao}<br/>
-           <span> R$ {preco}</span></p>
-        </div>
-    )
+function ItemPratos({ nome, categoria, preco, descricao, imagem }) {
+  return (
+    <div className="div-container-produtos">
+      <img src={imagem} alt={nome} />
+      <div className="container-padding">
+        <h3>{nome}</h3>
+        <p className="paragrafo">
+          <span className="span-categoria">{categoria}</span>
+
+          <span className="span-descricao">{descricao}</span>
+
+          <span className="span-preco"> R$ {preco}</span>
+        </p>
+      </div>
+    </div>
+  );
 }
 export default ItemPratos;
