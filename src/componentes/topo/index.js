ab9dc9992056cb3cac2fa21c946bfb9038140473
Renanluizssx <renanluizssx@gmail.com>
2023-12-18 04:42:14 -0300

    commitado

--- src/componentes/topo/index.js
+++ src/componentes/topo/index.js
@@ -1,17 +1,18 @@
 import "./topo.css";
-function Topo () {
-    return (
-        <header>
-            <div className="background-topo"> 
-            
-                <h1>RESTAURANT</h1>
-                <p>De pratos clássicos a criações surpreendentes,<br/> 
-                    nosso cardápio é um seguinte de sabores<br/> refinados.
-                </p>
-
-            </div>
-        </header>
-    )
+function Topo() {
+  return (
+    <header>
+      <div className="background-topo">
+        <h1>RESTAURANT</h1>
+        <p>
+          De pratos clássicos a criações surpreendentes,
+          <br />
+          nosso cardápio é um seguinte de sabores
+          <br /> refinados.
+        </p>
+      </div>
+    </header>
+  );
 }
 
 export default Topo;
